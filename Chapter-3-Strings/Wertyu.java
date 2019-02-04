c9fc555173c4af8f9a11b6cb2bbd606f86342e18
Christian Rivero Valencia <42509107+christianrivero@users.noreply.github.com>
2019-02-04 17:03:51 -0500

    Create Wertyu.java

---
+++ Chapter-3-Strings/Wertyu.java
@@ -0,0 +1,35 @@
+public class Wertyu
+{
+	public static void main(String[] args)
+	{
+		String filas[] = {"`1234567890-=", "QWERTYUIOP[]\\", "ASDFGHJKL;'", "ZXCVBNM,./"};
+		
+		char x='A';
+
+		System.out.println(retornaCaracterAnterior(x));
+	}
+
+	static char retornaCaracterAnterior(char original)
+	{
+		String filas[] = {"`1234567890-=", "QWERTYUIOP[]\\", "ASDFGHJKL;'", "ZXCVBNM,./"};
+		int i, j, palabra=0, caracter=0;//Almacena posiciones de palabra y caracter; deben inicializarse para no mostrar error.
+		char anterior;
+
+		for(i=0; i<filas.length; i++)
+		{
+			for(j=0; j<filas[i].length(); j++)
+			{
+				if(original==filas[i].charAt(j))
+				{
+					palabra=i;
+					caracter=j;
+				}
+			}
+		}
+
+		anterior = filas[palabra].charAt(caracter-1);
+
+		return anterior;
+	}
+
+}
