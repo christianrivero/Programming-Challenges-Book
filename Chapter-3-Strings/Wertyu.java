d47fc92357fe0fd1f510b9ab5b32606c561dd5fe
Christian Rivero Valencia <42509107+christianrivero@users.noreply.github.com>
2019-02-09 22:22:24 -0500

    Update Wertyu.java

--- Chapter-3-Strings/Wertyu.java
+++ Chapter-3-Strings/Wertyu.java
@@ -2,8 +2,7 @@ public class Wertyu
 {
 	public static void main(String[] args)
 	{
-		String filas[] = {"`1234567890-=", "QWERTYUIOP[]\\", "ASDFGHJKL;'", "ZXCVBNM,./"};
-		
+	   String filas[] = {"`1234567890-=", "QWERTYUIOP[]\\", "ASDFGHJKL;'", "ZXCVBNM,./"};
 		char x='A';
 
 		System.out.println(retornaCaracterAnterior(x));
