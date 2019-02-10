aa3d893b86fd4f88bcee0480a758f1b9df0c3459
Christian Rivero <christian.riverov10@gmail.com>
2019-02-10 01:23:22 -0500

    Where is Waldorf

--- Chapter-3-Strings/Where_is_Waldorf.cpp
+++ Chapter-3-Strings/Where_is_Waldorf.cpp
@@ -59,11 +59,24 @@ int validarEnteroPositivo()
    return int(n);
 }
 
+int validar_entre(int minimo, int maximo)
+{
+   float n;
+
+   do{
+      cin>>n;
+      if(minimo>=n || (n>=maximo))
+         cout<<"Debe ser un valor entre "<<minimo<<" y "<<maximo<<"."<<endl;
+   }while(minimo>=n || (n>=maximo));
+
+   return int(n);
+}
+
 
 main()
 {
-   string mxn, linea_blanco, palabra_fila;
-   int cases, m, n, i;
+   string mxn, linea_blanco, fila;
+   int cases, m, n, i, j, k;//k: cantidad de palabras a buscar.
 
    cases = validarEnteroPositivo();
 
@@ -79,15 +92,48 @@ main()
       if(1>n || n>50 || 1>m || m>50)
          cout<<"Los valores de m y n deben estar entre 1 y 50."<<endl;
    }while(1>n || n>50 || 1>m || m>50);
+
+   //Creacion de la matriz:
+   char matriz[m][n];
    
+   //Asignando cada palabra a cada fila de la matriz:
    for(i=0; i<m; i++)
    {
-      cout<<"palabra_fila ";
-      getline(cin, palabra_fila);
+      cout<<"fila["<<i<<"]: ";
+      getline(cin, fila);
+
+      for(j=0; j<n; j++)
+      {
+         matriz[i][j] = toupper(fila[j]);//pasar a mayúscula para estandarizar.
+      }
    }
 
+   //Imprimir Matriz:
    for(i=0; i<m; i++)
    {
-      cout<<palabra_fila<<endl;
+      for(j=0; j<n; j++)
+      {
+         cout<<matriz[i][j]<<" ";
+      }
+      cout<<endl;
+   }
+
+   k = validar_entre(1, 20);
+
+   cin.get();//Para evitar que se quede la tecla enter en el buffer.
+
+   //Crear arreglo de palabras a buscar en la matriz:
+   string palabras[k];
+
+   for(i=0; i<k; i++)
+   {
+      cout<<"palabras["<<i<<"]: ";
+      getline(cin, palabras[i]);
+   }
+
+   //Imprimir lista de palabras a buscar:
+   for(i=0; i<k; i++)
+   {
+      cout<<palabras[i]<<endl;
    }
 }
